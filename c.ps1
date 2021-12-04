90def5a2386923d6894cb2699b5f33512af85af4
G. R. Rocha <contato@glauco.rocks>
2021-12-04 18:01:19 -0300

    Updated c.ps1
    Added two conditionals that test whether there's a bin or an obj directory in the repository's root and, if don't, create it, before compiling anything.

--- c.ps1
+++ c.ps1
@@ -1,1 +1,11 @@
+if (-not (Test-Path $PSScriptRoot/bin))
+{
+    New-Item -ItemType "directory" -Name "bin" -Path $PSScriptRoot
+}
+
+if (-not (Test-Path $PSScriptRoot/obj))
+{
+    New-Item -ItemType "directory" -Name "obj" -Path $PSScriptRoot
+}
+
 cl /Fe:bin\prtstr.exe /Fo:obj\prtstr.obj src\main.c
